67c6bdc99f67c1277f0e20f4eeefc208252c3395
Toni <31803194+Pickausernaame@users.noreply.github.com>
2020-02-13 20:24:29 +0300

    New json format
    Проверено, работает, подтверждено неоднократно!

--- UDPServer/main.go
+++ UDPServer/main.go
@@ -16,12 +16,12 @@ import (
 const (
 	HOST = "192.168.100.240"
 	PORT = "41234"
-	BATCH_CAPACITY = 500
+	BATCH_CAPACITY = 1
 	NAME_OF_ACTION = "Basketball"
-	URL = "http://192.168.100.233:8000/series"
-	//URL = "http://localhost:8008/series"
+	//URL = "http://192.168.100.233:8000/series"
+	URL = "http://localhost:8008/series"
 	TOKEN = "854362ef8cf672ccb4254cac3e867ae8d7110dd2"
-	PAGE_SIZE = 100
+	PAGE_SIZE = 10
 	ACC_X = 0
 	ACC_Y = 1
 	ACC_Z = 2
@@ -31,34 +31,53 @@ const (
 	TIME = 6
 )
 
+// TODO ########### БЫЛО ##########
+JSON := `
+{
+	name: "Basketball"(string),
+	data: 	{
+				accX: [](float64),
+				accY: [](float64),
+				accZ: [](float64),
+				gyroX: [](float64),
+				gyroY: [](float64),
+				gyroZ: [](float64),
+				time: [](timestamp),
+			},
+}`
+
+// TODO ########### СТАЛО ##########
+JSON := `
+{
+	name: "Basketball"(string),
+	data: 	[{
+				accX: (float64),
+				accY: (float64),
+				accZ: (float64),
+				gyroX: (float64),
+				gyroY: (float64),
+				gyroZ: (float64),
+				time: (timestamp),
+			}],
+}`
 
 
-////type Data struct {
-////	accX opt.Float64 `json:"accX"`
-////	accY opt.Float64 `json:"accY"`
-////	accZ opt.Float64 `json:"accZ"`
-////	gyroX opt.Float64 `json:"gyroX"`
-////	gyroY opt.Float64 `json:"gyroY"`
-////	gyroZ opt.Float64 `json:"gyroZ"`
-////}
-//
-//type Batch struct {
-//	dataname string `json:"name"`
-//	dataArray []Data `json:"data"`
-//}
-
-type Data struct {
-	Name  string 		`json:"name"`
-	AccX  []float64  	`json:"accX"`
-	AccY  []float64  	`json:"accY"`
-	AccZ  []float64  	`json:"accZ"`
-	GyroX []float64  	`json:"gyroX"`
-	GyroY []float64  	`json:"gyroY"`
-	GyroZ []float64  	`json:"gyroZ"`
-	Time  []time.Time	`json:"time"`
+type Batch struct {
+	Dataname string `json:"name"`
+	DataArray []Data `json:"data"`
 }
 
-var GLOBAL_BATCH = make([]Data, PAGE_SIZE)
+type Data struct {
+	AccX  float64  	`json:"accX"`
+	AccY  float64  	`json:"accY"`
+	AccZ  float64  	`json:"accZ"`
+	GyroX float64  	`json:"gyroX"`
+	GyroY float64  	`json:"gyroY"`
+	GyroZ float64  	`json:"gyroZ"`
+	Time  time.Time	`json:"time"`
+}
+
+var GLOBAL_BATCH = make([]Batch, PAGE_SIZE)
 
 var GLOBAL_COUNTER = 0
 var LOCAL_COUNTER = 0
@@ -76,43 +95,43 @@ func handleUDPConnection(conn *net.UDPConn) {
 		data := strings.Split(string(buffer), ";")
 
 
-		GLOBAL_BATCH[GLOBAL_COUNTER].AccX[LOCAL_COUNTER], err = strconv.ParseFloat(data[ACC_X], 64)
+		GLOBAL_BATCH[GLOBAL_COUNTER].DataArray[LOCAL_COUNTER].AccX, err = strconv.ParseFloat(data[ACC_X], 64)
 		if err != nil {
 			fmt.Println(ACC_X)
 			log.Fatal(err)
 			return
 		}
-		GLOBAL_BATCH[GLOBAL_COUNTER].AccY[LOCAL_COUNTER], err = strconv.ParseFloat(data[ACC_Y], 64)
+		GLOBAL_BATCH[GLOBAL_COUNTER].DataArray[LOCAL_COUNTER].AccY, err = strconv.ParseFloat(data[ACC_Y], 64)
 		if err != nil {
 			fmt.Println(ACC_Y)
 			log.Fatal(err)
 			return
 		}
-		GLOBAL_BATCH[GLOBAL_COUNTER].AccZ[LOCAL_COUNTER], err = strconv.ParseFloat(data[ACC_Z], 64)
+		GLOBAL_BATCH[GLOBAL_COUNTER].DataArray[LOCAL_COUNTER].AccZ, err = strconv.ParseFloat(data[ACC_Z], 64)
 		if err != nil {
 			fmt.Println(ACC_Z)
 			log.Fatal(err)
 			return
 		}
-		GLOBAL_BATCH[GLOBAL_COUNTER].GyroX[LOCAL_COUNTER], err = strconv.ParseFloat(data[GYRO_X], 64)
+		GLOBAL_BATCH[GLOBAL_COUNTER].DataArray[LOCAL_COUNTER].GyroX, err = strconv.ParseFloat(data[GYRO_X], 64)
 		if err != nil {
 			fmt.Println(GYRO_X)
 			log.Fatal(err)
 			return
 		}
-		GLOBAL_BATCH[GLOBAL_COUNTER].GyroY[LOCAL_COUNTER], err = strconv.ParseFloat(data[GYRO_Y], 64)
+		GLOBAL_BATCH[GLOBAL_COUNTER].DataArray[LOCAL_COUNTER].GyroY, err = strconv.ParseFloat(data[GYRO_Y], 64)
 		if err != nil {
 			fmt.Println(GYRO_Y)
 			log.Fatal(err)
 			return
 		}
-		GLOBAL_BATCH[GLOBAL_COUNTER].GyroZ[LOCAL_COUNTER], err = strconv.ParseFloat(data[GYRO_Z], 64)
+		GLOBAL_BATCH[GLOBAL_COUNTER].DataArray[LOCAL_COUNTER].GyroZ, err = strconv.ParseFloat(data[GYRO_Z], 64)
 		if err != nil {
 			fmt.Println(GYRO_Z)
 			log.Fatal(err)
 			return
 		}
-		GLOBAL_BATCH[GLOBAL_COUNTER].Time[LOCAL_COUNTER] = time.Now()
+		GLOBAL_BATCH[GLOBAL_COUNTER].DataArray[LOCAL_COUNTER].Time = time.Now()
 		//data[TIME] = strings.Replace(data[TIME], "\x00", "",-1)
 		//GLOBAL_BATCH[GLOBAL_COUNTER].time[LOCAL_COUNTER], err = strconv.Atoi(data[TIME])
 		//if err != nil {
@@ -140,41 +159,31 @@ func handleUDPConnection(conn *net.UDPConn) {
 	}
 }
 
-func initBatch(name string, capacity int) (d *Data){
-	d = &Data{}
-	d.Name = name
-	d.AccX = make([]float64, capacity)
-	d.AccY = make([]float64, capacity)
-	d.AccZ = make([]float64, capacity)
-	d.GyroX = make([]float64, capacity)
-	d.GyroY = make([]float64, capacity)
-	d.GyroZ = make([]float64, capacity)
-	d.Time = make([]time.Time, capacity)
-	return
+func initBatch(name string, capacity int) (d *Batch) {
+	d = &Batch {
+		Dataname: name,
+		DataArray: make([]Data, capacity),
+	}
+	return d
 }
 
-func(d *Data) mock() {
-	for i := 0; i < len(d.AccX); i++ {
-		d.AccX[i] = rand.Float64()
-		d.AccY[i] = rand.Float64()
-		d.AccZ[i] = rand.Float64()
+func(d *Batch) mock() {
+	for i := 0; i < len(d.DataArray); i++ {
+		d.DataArray[i].AccX = rand.Float64()
+		d.DataArray[i].AccY = rand.Float64()
+		d.DataArray[i].AccZ = rand.Float64()
 
-		d.GyroX[i] = rand.Float64()
-		d.GyroY[i] = rand.Float64()
-		d.GyroZ[i] = rand.Float64()
+		d.DataArray[i].GyroX = rand.Float64()
+		d.DataArray[i].GyroY = rand.Float64()
+		d.DataArray[i].GyroZ = rand.Float64()
 	}
 }
 
-func (d *Data) clear() {
-	d.AccX = nil
-	d.AccY = nil
-	d.AccZ = nil
-	d.GyroX = nil
-	d.GyroY = nil
-	d.GyroZ = nil
+func (d *Batch) clear() {
+	d.DataArray = nil
 }
 
-func SendBatch(d *Data) {
+func SendBatch(d *Batch) {
 	client := http.Client{}
 	var buf bytes.Buffer
 	err := json.NewEncoder(&buf).Encode(&d)
@@ -183,11 +192,12 @@ func SendBatch(d *Data) {
 	}
 	b, _ := json.Marshal(d)
 	//fmt.Println(bytes.NewBuffer(b))
+
 	req, err := http.NewRequest("POST", URL, bytes.NewBuffer(b))
 	if err != nil {
 		log.Fatal("SENDING BATCH ERROR", err)
 	}
-
+	fmt.Println("SENDING BATCH")
 	req.Header.Set("Authorization", "Token " + TOKEN)
 	req.Header.Add("Content-Type", "application/json")
 	_, err = client.Do(req)
@@ -201,19 +211,24 @@ func SendBatch(d *Data) {
 func main()  {
 	for i := 0; i < len(GLOBAL_BATCH); i++ {
 		GLOBAL_BATCH[i] = *initBatch(NAME_OF_ACTION, BATCH_CAPACITY)
-		//GLOBAL_BATCH[i].mock()
+		GLOBAL_BATCH[i].mock()
 	}
 
-	udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%s", HOST, PORT))
-	if err != nil {
-		log.Fatal(err)
-	}
-	conn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	keepListening := true
-	for keepListening {
-			handleUDPConnection(conn)
+	//udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%s", HOST, PORT))
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//conn, err := net.ListenUDP("udp", udpAddr)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//keepListening := true
+	//for keepListening {
+	//		handleUDPConnection(conn)
+	//
+	//}
+	for i:=0; i < PAGE_SIZE; i++ {
+		SendBatch(&GLOBAL_BATCH[i])
+		time.Sleep(time.Second * 10)
 	}
 }
--- VovaServerMock/main.go
+++ VovaServerMock/main.go
@@ -13,36 +13,28 @@ const (
 
 )
 
-type Data struct {
-	Name  string 		`json:"name"`
-	AccX  []float64  	`json:"accX"`
-	AccY  []float64  	`json:"accY"`
-	AccZ  []float64  	`json:"accZ"`
-	GyroX []float64  	`json:"gyroX"`
-	GyroY []float64  	`json:"gyroY"`
-	GyroZ []float64  	`json:"gyroZ"`
-	Time  []time.Time	`json:"time"`
-}
-
-
-//type Data struct {
-//	accX opt.Float64 `json:"accX"`
-//	accY opt.Float64 `json:"accY"`
-//	accZ opt.Float64 `json:"accZ"`
-//	gyroX opt.Float64 `json:"gyroX"`
-//	gyroY opt.Float64 `json:"gyroY"`
-//	gyroZ opt.Float64 `json:"gyroZ"`
-//}
-
 type Batch struct {
-	dataname string `json:"name"`
-	dataArray []Data `json:"data"`
+	Dataname string `json:"name"`
+	DataArray []Data `json:"data"`
 }
+
+type Data struct {
+	Name  string 	`json:"name"`
+	AccX  float64  	`json:"accX"`
+	AccY  float64  	`json:"accY"`
+	AccZ  float64  	`json:"accZ"`
+	GyroX float64  	`json:"gyroX"`
+	GyroY float64  	`json:"gyroY"`
+	GyroZ float64  	`json:"gyroZ"`
+	Time  time.Time	`json:"time"`
+}
+
+
 
 var GLOBAL_COUNTER = 0
 
 func BatchTaker(c *gin.Context) {
-	data := Data{}
+	data := Batch{}
 	_ = json.NewDecoder(c.Request.Body).Decode(&data)
 	fmt.Println(data)
 }
