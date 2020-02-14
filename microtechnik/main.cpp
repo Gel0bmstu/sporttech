a5892548df0c9af9d9f5de434bd3cde29bbdc2c5
Buzov Vyahceslav <buzovv1997@gmail.com>
2020-02-14 20:05:50 +0300

    implemented EEPROM in main

--- microtechnik/main.cpp
+++ microtechnik/main.cpp
@@ -4,11 +4,71 @@
 #include <Wire.h>
 #include <TroykaIMU.h>
 
-const char * networkName = "MGTS_30";
-const char * networkPswd = "mgtswifi30";
+#include <EEPROM.h>
 
-const char * udpAddress = "192.168.100.240";
-const int udpPort = 41234;
+struct {
+  char ssid[20];
+  char passwd[20];
+} wifi;
+
+struct {
+  char ip[16];
+  uint port;
+} server;
+
+uint WIFI_ADDR = 0;
+uint SERVER_ADDR = sizeof(wifi);
+
+void get_settings()
+{
+  EEPROM.get(WIFI_ADDR, wifi);
+  EEPROM.get(SERVER_ADDR, server);
+}
+
+void put_settings()
+{
+  EEPROM.put(WIFI_ADDR, wifi);
+  EEPROM.put(SERVER_ADDR, server);
+  EEPROM.commit();
+}
+
+void print_wifi()
+{
+  EEPROM.get(WIFI_ADDR,wifi);
+  Serial.println("WIFI:");
+  Serial.println("ssid: "+String(wifi.ssid));
+  Serial.println("password: "+String(wifi.passwd));
+}
+
+void print_server()
+{
+  EEPROM.get(SERVER_ADDR,server);
+  Serial.println("SERVER:");
+  Serial.println("ip: "+String(server.ip));
+  Serial.println("port: "+String(server.port));
+}
+
+void print_settings()
+{
+  print_wifi();
+  Serial.println();
+  print_server();
+}
+
+bool parse_settings(String raw_settings)
+{
+  char buf[50];
+  bool condition=false;
+  raw_settings.toCharArray(buf, 50);
+
+  if(sscanf(buf,"wifi '%[^']' '%[^']'",&wifi.ssid,&wifi.passwd))
+    EEPROM.put(WIFI_ADDR, wifi);
+
+  if(sscanf(buf,"server '%[^']' %u",&server.ip,&server.port))
+    EEPROM.put(SERVER_ADDR, server);
+
+  EEPROM.commit();
+}
 
 boolean connected = false;
 
@@ -52,12 +112,12 @@ void WiFiEvent(WiFiEvent_t event){
     }
 }
 
-void connectToWiFi(const char * ssid, const char * pwd){
-  Serial.println("Connecting to WiFi network: " + String(ssid));
+void connectToWiFi(const char * wifi.ssid, const char * wifi.passwd){
+  Serial.println("Connecting to WiFi network: " + String(wifi.ssid));
 
   WiFi.disconnect(true);
   WiFi.onEvent(WiFiEvent);
-  WiFi.begin(ssid, pwd);
+  WiFi.begin(wifi.ssid, wifi.passwd);
 
   Serial.println("Waiting for WIFI connection...");
 }
@@ -65,7 +125,10 @@ void connectToWiFi(const char * ssid, const char * pwd){
 void setup(){
   Serial.begin(115200);
 
-  connectToWiFi(networkName, networkPswd);
+  EEPROM.begin(512);
+  get_settings();
+
+  connectToWiFi(wifi.ssid, wifi.passwd);
 
   gyro.begin();
   accel.begin();
@@ -73,7 +136,7 @@ void setup(){
 
 void loop(){
   if(connected){
-    udp.beginPacket(udpAddress,udpPort);
+    udp.beginPacket(server.ip, server.port);
     udp.printf(zalupa());
     udp.endPacket();
   }
