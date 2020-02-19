5ece2b195d1dc1517a5b0553500d1370e868c902
Temirlan <rakhimgaliev56@gmail.com>
2020-02-20 02:00:21 +0300

    now it can call nfc conn trigger

--- app/src/main/java/com/example/sporttech/MainActivity.java
+++ app/src/main/java/com/example/sporttech/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.sporttech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter != null && nfcAdapter.isEnabled()) {
+//            Toast.makeText(this, "NFC is available!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "NFC is not available!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Toast.makeText(this, "NFC intent recieved!", Toast.LENGTH_LONG).show();
+
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 ,intent, 0);
+        IntentFilter[] intentFilters = new IntentFilter[]{};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        nfcAdapter.disableForegroundDispatch(this);
+
+        super.onPause();
     }
 }
