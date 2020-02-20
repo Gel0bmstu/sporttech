8ddb6f8e77f9e3f6e97f50e28a87a147ff8c5f50
Temirlan <rakhimgaliev56@gmail.com>
2020-02-20 17:18:30 +0300

    it can write ndef message to nfc card

--- app/src/main/java/com/example/sporttech/MainActivity.java
+++ app/src/main/java/com/example/sporttech/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -29,28 +34,94 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onNewIntent(Intent intent) {
-        Toast.makeText(this, "NFC intent recieved!", Toast.LENGTH_LONG).show();
-
         super.onNewIntent(intent);
+
+        if (intent.hasExtra(NfcAdapter.EXTRA_TAG)) {
+            Toast.makeText(this, "NFC intent recieved!", Toast.LENGTH_LONG).show();
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            NdefMessage ndefMessage = createNdefMessage("My string content!");
+
+            writeNdefMessage(tag, ndefMessage);
+        }
     }
 
     @Override
     protected void onResume() {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 ,intent, 0);
-        IntentFilter[] intentFilters = new IntentFilter[]{};
-
-        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
-
         super.onResume();
+        enableForegroundDispatch();
     }
 
     @Override
     protected void onPause() {
-        nfcAdapter.disableForegroundDispatch(this);
-
         super.onPause();
+        disableForegroundDispatch();
+    }
+
+    private void enableForegroundDispatch() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter[] intentFilters = new IntentFilter[]{};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+
+    }
+
+    private void disableForegroundDispatch() {
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void formatTag(Tag tag, NdefMessage ndefMessage) {
+        try {
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+
+            if (ndefFormatable == null) {
+                Toast.makeText(this, "Tag is not ndef formatable!", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            ndefFormatable.connect();
+            ndefFormatable.format(ndefMessage);
+            ndefFormatable.close();
+
+            Toast.makeText(this, "Tag written!", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Log.e("formatTag", e.toString());
+        }
+    }
+
+    private void writeNdefMessage(Tag tag, NdefMessage ndefMessage) {
+        try {
+            if (tag == null) {
+                Toast.makeText(this, "Tag is not ndef formatable!", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Ndef ndef = Ndef.get(tag);
+
+            if (ndef == null) {
+                formatTag(tag, ndefMessage);
+            } else {
+                ndef.connect();
+                if (!ndef.isWritable()) {
+                    Toast.makeText(this, "Tag is not writable!", Toast.LENGTH_LONG).show();
+                    ndef.close();
+                    return;
+                }
+
+                ndef.writeNdefMessage(ndefMessage);
+                ndef.close();
+                Toast.makeText(this, "Tag written!", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Log.e("writeNdefMessage", "some error");
+        }
+    }
+
+    private NdefMessage createNdefMessage(String content) {
+        NdefRecord ndefRecord = NdefRecord.createTextRecord("UTF-8", content);
+        return new NdefMessage(new NdefRecord[]{ndefRecord});
     }
 }
